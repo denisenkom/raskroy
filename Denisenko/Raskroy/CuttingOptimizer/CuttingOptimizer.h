086e150c683b8c443f949eb6d44e6afb8a3ae7fa
Миша <@localhost>
2006-01-21 19:48:23 +0000

    Vss2Git

--- Denisenko/Raskroy/CuttingOptimizer/CuttingOptimizer.h
+++ Denisenko/Raskroy/CuttingOptimizer/CuttingOptimizer.h
@@ -2,25 +2,54 @@
 
 #pragma once
 
-#include "../raskroy.h"
+#include "raskroy.h"
 
 using namespace std;
 using namespace System;
 using namespace Denisenko::Raskroy;
 
 namespace Denisenko {
-namespace CuttingOptimizer {
+namespace Cutting {
+namespace Optimizing {
 
 	public ref class Optimizer
 	{
 	public:
 
+		ref class ParametersCollection
+		{
+		public:
+			property Int32 CutterThickness
+			{
+				Int32 get() { return m_cutterThickness; }
+				void set(Int32 value) { m_cutterThickness = value; }
+			}
+
+			Int32 CutOffTop;
+			Int32 CutOffBottom;
+			Int32 CutOffLeft;
+			Int32 CutOffRight;
+
+		internal:
+			ParametersCollection()
+			{
+				m_cutterThickness = 4;
+				CutOffTop = 12;
+				CutOffBottom = 12;
+				CutOffLeft = 12;
+				CutOffRight = 12;
+			}
+
+		private:
+			Int32 m_cutterThickness;
+		};
+
 		Optimizer()
 		{
 			m_started = false;
-			m_cutterThickness = 4.4;
 			m_parts = gcnew PartsCollection();
 			m_sheets = gcnew SheetsCollection();
+			m_parameters = gcnew ParametersCollection();
 			m_raskroy = new Denisenko::Raskroy::Raskroy();
 		}
 
@@ -29,58 +58,79 @@ namespace CuttingOptimizer {
 			delete m_raskroy;
 		}
 
-		property Double CutterThickness
-		{
-			Double get() { return m_cutterThickness; }
-			void set(Double value) { m_cutterThickness = value; }
-		}
-
 		property PartsCollection^ Parts { PartsCollection^ get() { return m_parts; } }
 		property SheetsCollection^ Sheets { SheetsCollection^ get() { return m_sheets; } }
+		property ParametersCollection^ Parameters { ParametersCollection^ get() { return m_parameters; } }
+		property CuttingResult^ CurrentResult { CuttingResult^ get() { return m_result; } }
 
 		void Reset()
 		{
 			m_started = false;
 		}
 
-		Boolean NextResult([out] CuttingResult^ cuttingResult)
+		Boolean NextResult() 
 		{
 			if(!m_started)
 			{
 				Denisenko::Raskroy::Parts convParts = ConvertParts();
 				Denisenko::Raskroy::Parts convSheets = ConvertSheets();
+				m_raskroy->put_SawThickness(Parameters->CutterThickness);
 				m_raskroy->Begin(convParts, convSheets);
 				m_started = true;
 			}
 			Denisenko::Raskroy::t_result result;
-			bool retVal = m_raskroy->NextResult(result);
-			cuttingResult = ConvertResult(result);
-			return retVal;
+			if(m_raskroy->NextResult(result))
+			{
+				m_result = ConvertResult(result);
+				return true;
+			}
+			else
+			{
+				m_result = nullptr;
+				return false;
+			}
 		}
 
 	private:
 		Denisenko::Raskroy::Raskroy* m_raskroy;
-		Double m_cutterThickness;
 		PartsCollection^ m_parts;
 		SheetsCollection^ m_sheets;
 		Boolean m_started;
+		ParametersCollection^ m_parameters;
+		CuttingResult^ m_result;
 
 		Denisenko::Raskroy::Parts ConvertParts()
 		{
 			Denisenko::Raskroy::Parts result;
+			for each(Part^ part in Parts)
+			{
+				result.push_back(Denisenko::Raskroy::Part(
+					part->Length,
+					part->Width,
+					part->CanRotate,
+					part->Quantity));
+			}
 			return result;
 		}
 
 		Denisenko::Raskroy::Parts ConvertSheets()
 		{
 			Denisenko::Raskroy::Parts result;
+			for each(Sheet^ sheet in Sheets)
+			{
+				result.push_back(Denisenko::Raskroy::Part(
+					sheet->Length,
+					sheet->Width));
+			}
 			return result;
 		}
 
 		CuttingResult^ ConvertResult(const Denisenko::Raskroy::t_result& result)
 		{
-			return nullptr;
+			CuttingResultBuilder^ builder = gcnew CuttingResultBuilder();
+			return builder->Convert(result, Parameters->CutterThickness, result.sheet->Rect.Length, result.sheet->Rect.Width);
 		}
 	};
+}
 }
 }
