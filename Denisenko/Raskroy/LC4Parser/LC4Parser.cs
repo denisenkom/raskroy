ee283056cc43a7b6c38b28a33f7dcda56da386c0
Миша <@localhost>
2006-03-07 06:17:25 +0000

    Vss2Git

--- Denisenko/Raskroy/LC4Parser/LC4Parser.cs
+++ Denisenko/Raskroy/LC4Parser/LC4Parser.cs
@@ -22,21 +22,21 @@ namespace Denisenko.Cutting.LC4
 
 		private void LoadSheet()
 		{
-			LC4Sheet sheet = m_document.CreateSheet();
+			LC4Sheet sheet = new LC4Sheet();
 			Byte[] pad;
 			pad = ReadBytes(11);
-			String someString1 = ReadString();
-			String someString2 = ReadString();
-			String someString3 = ReadString();
+			sheet.SomeString1 = ReadString();
+			sheet.SomeString2 = ReadString();
+			sheet.SomeString3 = ReadString();
 			sheet.Size1 = ReadNumeric();
 			sheet.Size2 = ReadNumeric();
 			sheet.Thickness = ReadNumeric();
-			Int32 someInteger1 = ReadInt32();
-			Int32 someInteger2 = ReadInt32();
+			sheet.SomeInteger1 = ReadInt32();
+			sheet.SomeInteger2 = ReadInt32();
 			pad = ReadBytes(2);
-			Int32 someInteger3 = ReadInt32();
+			sheet.SomeInteger3 = ReadInt32();
 			pad = ReadBytes(10);
-			Int32 someInteger4 = ReadInt32();
+			sheet.SomeInteger4 = ReadInt32();
 			pad = ReadBytes(3);
 			m_document.Sheets.Add(sheet);
 		}
@@ -44,85 +44,47 @@ namespace Denisenko.Cutting.LC4
 		private void SaveSheet(LC4Sheet sheet)
 		{
 			WriteBytes(m_sheetPreambule);
-			WriteString("");
-			WriteString("");
-			WriteString("");
+			WriteString(sheet.SomeString1);
+			WriteString(sheet.SomeString2);
+			WriteString(sheet.SomeString3);
 			WriteNumeric(sheet.Size1);
 			WriteNumeric(sheet.Size2);
 			WriteNumeric(sheet.Thickness);
-			WriteInt32(1); // some integer 1
-			WriteInt32(0); // some integer 2
+			WriteInt32(sheet.SomeInteger1);
+			WriteInt32(sheet.SomeInteger2);
 			WriteBytes(new Byte[] { 19, 0 });
-			WriteInt32(0); // some integer 3
+			WriteInt32(sheet.SomeInteger3);
 			WriteBytes(new Byte[] { 1, 0, 0 });
 			WriteBytes(m_middlePad);
-			WriteInt32(0); // some integer 4
+			WriteInt32(sheet.SomeInteger4);
 			WriteBytes(new Byte[] { 1, 0, 1 }); // teil
 		}
 
-		private LC4SectionType TypeFromString(String sectionType)
+		private void LoadSection(List<LC4Section> parentCollection)
 		{
-			switch (sectionType)
-			{
-				case "Anschnitt":
-					return LC4SectionType.Scrap;
-				case "Schnitt":
-					return LC4SectionType.Cut;
-				case "Teil":
-					return LC4SectionType.Detail;
-				case "Rest":
-					return LC4SectionType.Remain;
-				case "Streifen":
-					return LC4SectionType.NewLine;
-				default:
-					throw new ArgumentOutOfRangeException("sectionType", sectionType, "Invalid value");
-			}
-		}
-
-		private String StringFromType(LC4SectionType sectionType)
-		{
-			switch (sectionType)
-			{
-				case LC4SectionType.Scrap:
-					return "Anschnitt";
-				case LC4SectionType.Cut:
-					return "Schnitt";
-				case LC4SectionType.Detail:
-					return "Teil";
-				case LC4SectionType.Remain:
-					return "Rest";
-				case LC4SectionType.NewLine:
-					return "Streifen";
-				default:
-					throw new ArgumentOutOfRangeException("sectionType", sectionType, "Invalid value");
-			}
-		}
-
-		private void LoadSection(LC4SectionsCollection parentCollection)
-		{
-			LC4Section operation = m_document.CreateSection();
+			LC4Section operation = new LC4Section();
 
 			Byte[] pad;
 			pad = ReadBytes(7);
 
-			operation.SectionType = TypeFromString(ReadString());
-			String copyString = ReadString();
+			operation.SectionType = (LC4SectionType)Enum.Parse(typeof(LC4SectionType), ReadString());
+			operation.CopyString = ReadString();
 			operation.Size = ReadNumeric();
-			Int32 someInteger1 = ReadInt32();
+			operation.SomeInteger1 = ReadInt32();
 			pad = ReadBytes(2);
-			Int32 operationTypeCode = ReadInt32();
+			ReadInt32(); // skip type code
 			pad = ReadBytes(2);
-			Int32 someNumber2 = ReadInt32();
+			operation.SomeInteger2 = ReadInt32();
 			pad = ReadBytes(4);
-			Int32 someNumber3 = ReadInt32();
+			operation.SomeInteger3 = ReadInt32();
 			pad = ReadBytes(7);
-			Int32 someNumber4 = ReadInt32();
+			operation.SomeInteger4 = ReadInt32();
 
 			pad = ReadBytes(7);
 			Int32 subOperationsCount = ReadInt32();
 
 			for (Int32 i = 0; i < subOperationsCount; i++)
-				LoadSection(operation);
+				LoadSection(operation.NestedSections);
 
 			pad = ReadBytes(3);
 
@@ -132,30 +94,30 @@ namespace Denisenko.Cutting.LC4
 		private void SaveSection(LC4Section section)
 		{
 			WriteBytes(new Byte[] { 1, 0, 1, 5, 0, 2, 0 });
-			WriteString(StringFromType(section.SectionType));
-			WriteString("");
+			WriteString(section.SectionType.ToString());
+			WriteString(section.CopyString);
 			WriteNumeric(section.Size);
-			WriteInt32(0);
+			WriteInt32(section.SomeInteger1);
 			WriteBytes(new Byte[] { 17, 0 });
-			WriteInt32(0); // operation type
+			WriteInt32((Int32)section.SectionType); // operation type
 			WriteBytes(new Byte[] { 18, 0 });
-			WriteInt32(0);
+			WriteInt32(section.SomeInteger2);
 			WriteBytes(new Byte[] { 5, 0, 1, 0 });
-			WriteInt32(0);
+			WriteInt32(section.SomeInteger3);
 			WriteBytes(new Byte[] { 1, 0, 1, 5, 0, 1, 0 });
-			WriteInt32(0);
+			WriteInt32(section.SomeInteger4);
 			WriteBytes(new Byte[] { 1, 0, 1, 5, 0, 1, 0 });
-			WriteInt32(section.Count);
-			foreach (LC4Section nestedSection in section)
+			WriteInt32(section.NestedSections.Count);
+			foreach (LC4Section nestedSection in section.NestedSections)
 			{
 				SaveSection(nestedSection);
 			}
-			WriteBytes(new Byte[] { 1, 0, 1 });
+			WriteBytes(new Byte[] { 1, 0, 0 });
 		}
 
 		private LC4Variable LoadVariable()
 		{
-			LC4Variable result = m_document.CreateVariable();
+			LC4Variable result = new LC4Variable();
 
 			Byte[] pad;
 			pad = ReadBytes(7);
@@ -174,123 +136,122 @@ namespace Denisenko.Cutting.LC4
 			WriteBytes(new Byte[] { 1, 0, 0 });
 		}
 
-		private void LoadCut()
+		private void LoadCutting()
 		{
-			LC4Cutting cutting = m_document.CreateCutting();
+			LC4Cutting cutting = new LC4Cutting();
 
 			Byte[] pad;
 			pad = ReadBytes(11);
 			cutting.Name = ReadString();
-			String someString1 = ReadString();
+			cutting.SomeString1 = ReadString();
 			cutting.Size1 = ReadNumeric();
 
-			Int32 someInt = ReadInt32();
+			cutting.SomeInteger1 = ReadInt32();
 			pad = ReadBytes(0x2);
-			Int32 someInt2 = ReadInt32();
+			cutting.SomeInteger2 = ReadInt32();
 			pad = ReadBytes(0x2);
-			Int32 someInt3 = ReadInt32();
+			cutting.SomeInteger3 = ReadInt32();
 			pad = ReadBytes(0x4);
-			Int32 someInt4 = ReadInt32();
+			cutting.SomeInteger4 = ReadInt32();
 			pad = ReadBytes(0x7);
-			Int32 someInt5 = ReadInt32();
+			cutting.SomeInteger5 = ReadInt32();
 			pad = ReadBytes(0x3);
-			String someString2 = ReadString();
+			cutting.SomeString2 = ReadString();
 			pad = ReadBytes(0x8);
-			Int32 someInt6 = ReadInt32();
-			Int32 someInt7 = ReadInt32();
+			cutting.SomeInteger6 = ReadInt32();
+			cutting.SheetIndex = ReadInt32();
 			pad = ReadBytes(0x7);
-			Int32 someInt8 = ReadInt32();
+			cutting.SomeInteger8 = ReadInt32();
 
 			pad = ReadBytes(0x7);
 			Int32 operationsCount = ReadInt32();
 
 			for (Int32 i = 0; i < operationsCount; i++)
-				LoadSection(cutting);
+			{
+				LoadSection(cutting.Sections);
+			}
 			pad = ReadBytes(0x3);
 
 			cutting.Size2 = ReadNumeric();
-			Int32 someInt9 = ReadInt32();
-			Int32 someInt10 = ReadInt32();
-			String someString3 = ReadString();
+			cutting.SomeInteger9 = ReadInt32();
+			cutting.SomeInteger10 = ReadInt32();
+			cutting.SomeString3 = ReadString();
 
 			pad = ReadBytes(6);
-			Int32 someInt11 = ReadInt32();
+			cutting.SomeInteger11 = ReadInt32();
 			pad = ReadBytes(10);
 
 			Int32 variablesCount = ReadInt32();
-			if (variablesCount > 0)
+			for (Int32 i = 0; i < variablesCount; i++)
 			{
-				LC4VariablesCollection variables = m_document.CreateVariablesCollection();
-				for (Int32 i = 0; i < variablesCount; i++)
-					variables.Add(LoadVariable());
-				cutting.Variables = variables;
+				cutting.Variables.Add(LoadVariable());
 			}
 
 			m_document.Cuttings.Add(cutting);
 
-			Int32 someInt12 = ReadInt32();
+			cutting.SomeInteger12 = ReadInt32();
 			pad = ReadBytes(3);
-			Double someDouble1 = ReadDouble();
-			Double someDouble2 = ReadDouble();
-			Int32 someInt13 = ReadInt32();
-			Double someDouble3 = ReadDouble();
-			Double someDouble4 = ReadDouble();
-			Int32 someInt14 = ReadInt32();
-			Double someDouble5 = ReadDouble();
-			Double someDouble6 = ReadDouble();
+			cutting.ScrapsSquare = ReadDouble();
+			cutting.TotalSquare = ReadDouble();
+			cutting.RemainsCount = ReadInt32();
+			cutting.RemainsSquare = ReadDouble();
+			cutting.DustSquare = ReadDouble();
+			cutting.DetailsCount = ReadInt32();
+			cutting.DetailsSquare = ReadDouble();
+			cutting.ScrapPercent = ReadDouble();
 		}
 
 		private void SaveCutting(LC4Cutting cutting)
 		{
 			WriteBytes(m_cuttingPreambule);
 			WriteString(cutting.Name);
-			WriteString("");
+			WriteString(cutting.SomeString1);
 			WriteNumeric(cutting.Size1);
-			WriteInt32(1);
+			WriteInt32(cutting.SomeInteger1);
 			WriteBytes(new Byte[] { 17, 0 });
-			WriteInt32(1);
+			WriteInt32(cutting.SomeInteger2);
 			WriteBytes(new Byte[] { 18, 0 });
-			WriteInt32(1);
+			WriteInt32(cutting.SomeInteger3);
 			WriteBytes(new Byte[] { 5, 0, 1, 0 });
-			WriteInt32(1);
+			WriteInt32(cutting.SomeInteger4);
 			WriteBytes(new Byte[] { 1, 0, 1, 5, 0, 2, 0 });
-			WriteInt32(0);
+			WriteInt32(cutting.SomeInteger5);
 			WriteBytes(new Byte[] { 1, 0, 0 });
-			WriteString("");
+			WriteString(cutting.SomeString2);
 			WriteBytes(new Byte[] { 1, 0, 0, 1, 0, 1, 20, 0 });
-			WriteInt32(3);
-			WriteInt32(0);
+			WriteInt32(cutting.SomeInteger6);
+			WriteInt32(cutting.SheetIndex);
 			WriteBytes(new Byte[] { 1, 0, 1, 5, 0, 1, 0 });
-			WriteInt32(0);
+			WriteInt32(cutting.SomeInteger8);
 			WriteBytes(new Byte[] { 1, 0, 1, 5, 0, 1, 0 });
-			WriteInt32(cutting.Count);
-			foreach (LC4Section section in cutting)
+			WriteInt32(cutting.Sections.Count);
+			foreach (LC4Section section in cutting.Sections)
 			{
 				SaveSection(section);
 			}
 			WriteBytes(new Byte[] { 1, 0, 0 });
 			WriteNumeric(cutting.Size2);
-			WriteInt32(32);
-			WriteInt32(4);
-			WriteString("");
+			WriteInt32(cutting.SomeInteger9);
+			WriteInt32(cutting.SomeInteger10);
+			WriteString(cutting.SomeString3);
 			WriteBytes(new Byte[] { 5, 0, 1, 0, 16, 0 });
-			WriteInt32(0);
+			WriteInt32(cutting.SomeInteger11);
 			WriteBytes(new Byte[] { 1, 0, 0, 1, 0, 1, 5, 0, 1, 0 });
 			WriteInt32(cutting.Variables.Count);
 			foreach (LC4Variable variable in cutting.Variables)
 			{
 				SaveVariable(variable);
 			}
-			WriteInt32(0);
+			WriteInt32(cutting.SomeInteger12);
 			WriteBytes(new Byte[] { 1, 0, 0 });
-			WriteDouble(0.0);
-			WriteDouble(0.0);
-			WriteInt32(0);
-			WriteDouble(0.0);
-			WriteDouble(0.0);
-			WriteInt32(0);
-			WriteDouble(0.0);
-			WriteDouble(0.0);
+			WriteDouble(cutting.ScrapsSquare);
+			WriteDouble(cutting.TotalSquare);
+			WriteInt32(cutting.RemainsCount);
+			WriteDouble(cutting.RemainsSquare);
+			WriteDouble(cutting.DustSquare);
+			WriteInt32(cutting.DetailsCount);
+			WriteDouble(cutting.DetailsSquare);
+			WriteDouble(cutting.ScrapPercent);
 		}
 
 		public LC4Document Load(String fileName)
@@ -301,23 +262,27 @@ namespace Denisenko.Cutting.LC4
 			Byte[] pad;
 			pad = ReadBytes(7);
 			m_document.InternalName = ReadString();
-			m_document.Generator = ReadString();
+			m_document.Description = ReadString();
 
 			pad = ReadBytes(7);
-			Int32 someInteger1 = ReadInt32();
+			m_document.SomeInteger1 = ReadInt32();
 
 			pad = ReadBytes(7);
 			Int32 sheetsCount = ReadInt32();
 			for (Int32 i = 0; i < sheetsCount; i++)
+			{
 				LoadSheet();
+			}
 
 			pad = ReadBytes(7);
-			Int32 someInteger2 = ReadInt32();
+			m_document.SomeInteger2 = ReadInt32();
 
 			pad = ReadBytes(7);
 			Int32 cutsCount = ReadInt32();
 			for (Int32 i = 0; i < cutsCount; i++)
-				LoadCut();
+			{
+				LoadCutting();
+			}
 
 			pad = ReadBytes(13);
 			m_fileStream.Close();
@@ -331,9 +296,9 @@ namespace Denisenko.Cutting.LC4
 			m_fileStream = new FileStream(fileName, fileMode);
 			WriteBytes(m_header);
 			WriteString(m_document.InternalName);
-			WriteString(m_document.Generator);
+			WriteString(m_document.Description);
 			WriteBytes(m_middlePad);
-			WriteInt32(0); // some integer 1
+			WriteInt32(m_document.SomeInteger1);
 			WriteBytes(m_middlePad);
 			WriteInt32(document.Sheets.Count); // sheets count
 			foreach (LC4Sheet sheet in document.Sheets)
@@ -341,9 +306,9 @@ namespace Denisenko.Cutting.LC4
 				SaveSheet(sheet);
 			}
 			WriteBytes(m_middlePad);
-			WriteInt32(0); // some integer 1
+			WriteInt32(m_document.SomeInteger2);
 			WriteBytes(m_middlePad);
-			WriteInt32(document.Sheets.Count); // cuts count
+			WriteInt32(document.Cuttings.Count); // cuts count
 			foreach (LC4Cutting cutting in document.Cuttings)
 			{
 				SaveCutting(cutting);
