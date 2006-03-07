5c714638a34392c38c3d3de42ae7852066321579
Миша <@localhost>
2006-03-07 06:08:19 +0000

    Vss2Git

--- Denisenko/Raskroy/CuttingOptimizerTester/Program.cs
+++ Denisenko/Raskroy/CuttingOptimizerTester/Program.cs
@@ -7,7 +7,6 @@ using Denisenko;
 using Denisenko.Cutting;
 using Denisenko.Cutting.Optimizing;
 using Denisenko.Cutting.Converting;
-using Denisenko.Cutting.LC4;
 
 namespace CuttingOptimizerTester
 {
@@ -16,32 +15,34 @@ namespace CuttingOptimizerTester
 		static void Main(string[] args)
 		{
 			Optimizer optimizer = new Optimizer();
-			optimizer.Parts.Add(new Part(Decimal.Parse("200"), Decimal.Parse("300"), 3, false));
-			optimizer.Sheets.Add(new Sheet(Decimal.Parse("2730"), Decimal.Parse("1830")));
-			optimizer.Parameters.CutterThickness = Decimal.Parse("4.4", CultureInfo.InvariantCulture);
-			optimizer.Parameters.CutOffBottom = Decimal.Parse("13");
+
+			Part part = new Part();
+			part.Length = 200M;
+			part.Width = 200M;
+			part.Quantity = 3;
+			part.CanRotate = false;
+			optimizer.Parts.Add(part);
+
+			Sheet sheet = new Sheet();
+			sheet.Width = 2730M;
+			sheet.Height = 1830M;
+			sheet.Thickness = 16M;
+			optimizer.Sheets.Add(sheet);
+
+			optimizer.Parameters.CutterThickness = 4.4M;
+			optimizer.Parameters.CutOffBottom = 13M;
 
 			LC4Convertor convertor = new LC4Convertor();
-			Int32 index = 0;
+			List<CuttingScheme> schemes = new List<CuttingScheme>();
 			while (optimizer.NextResult())
 			{
-				convertor.AddCuttingResult(optimizer.CurrentResult, index.ToString());
-				index++;
+				schemes.Add(optimizer.CurrentResult);
 			}
-			LC4Document doc = convertor.Result;
-			doc.Save("converted.xml");
+			Denisenko.Cutting.LC4.LC4Document doc = convertor.Convert(schemes);
+			doc.Serialize("converted.xml");
 			doc.InternalName = "converted";
-			doc.Generator = "Manualy generated";
-			LC4Parser parser = new LC4Parser();
-			LC4Sheet sheet = doc.CreateSheet();
-			sheet.Size1 = LC4Numeric.FromNonScaled(1000);
-			sheet.Size2 = LC4Numeric.FromNonScaled(2000);
-			sheet.Thickness = LC4Numeric.FromNonScaled(16);
-			doc.Sheets.Add(sheet);
-			LC4Variable variable = doc.CreateVariable();
-			variable.Name = "Name";
-			variable.Value = "Value";
-			doc.Cuttings[0].Variables.Add(variable);
+			doc.Description = "Manualy generated";
+			Denisenko.Cutting.LC4.LC4Parser parser = new Denisenko.Cutting.LC4.LC4Parser();
 			parser.Save(@"..\..\..\..\Reigenering lc4\converted.lc4", FileMode.Create, doc);
 		}
 	}
