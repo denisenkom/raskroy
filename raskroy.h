f4ff996a07a796ab9330a017f7f1e1c274953b7a
Mikhail Denisenko <denisenkom@gmail.com>
2013-10-09 01:55:06 -0400

    reverted to latest version

--- raskroy.h
+++ raskroy.h
@@ -2,76 +2,50 @@
 #define RASKROY_H_INCLUDED
 
 #include <list>
+//#include <set>
+#include <vector>
 #include <memory>
-//#include <cassert>
+#include <exception>
+#include <assert.h>
+#include "types.hpp"
+
+// TODO: процент завершения
+// TODO: есть глюк в тесте 1 на странице 1
+
+#include "common.h"
+#include "perebor_2d.h"
 
 namespace raskroy {
 
-	typedef float scalar;
-	typedef struct {scalar size[2];} t_list;
-	typedef struct {unsigned id; t_list list; bool rotate;
-		unsigned ammount; unsigned material_id;} t_part;
-	typedef std::list<t_part> t_parts_list;
-	typedef struct {unsigned id; t_parts_list parts; t_parts_list lists;} t_material;
-	typedef std::list<t_material> t_materials_list;
-	typedef struct {scalar size; unsigned n; t_part *part;} t_other_size;
-	typedef std::list<t_other_size> t_other_sizes_list;
-	typedef struct {scalar size; t_other_sizes_list other_sizes;} t_size;
-	typedef std::list<t_size> t_sizes_list;
-
-	//typedef std::vector<unsigned n> t_other_sizes_result_list;
-	//typedef std::list<struct {unsigned size_i, unsigned s, t_other_sizes_result_list other_sizes}>
-	//typedef struct s_result {bool bylen; std::list<t_cut> cuts, std::list<s_result>} t_result;
-
-	typedef struct {scalar saw_thickness;} t_factory;
-	struct t_stat {
-		scalar sum_cut_length;
-		scalar sum_remain;
-		t_stat() : sum_cut_length(0), sum_remain(0) {}
-		t_stat& operator += (const t_stat& stat) {
-			sum_cut_length += stat.sum_cut_length;
-			sum_remain += stat.sum_remain;
-			return *this;
-		}
-	};
-
-	class criteria {
-	public:
-		// return true if a better than b
-		virtual bool compare(t_stat a, t_stat b)
-		{
-			if (a.sum_remain < b.sum_remain)
-				return true;
-			if (a.sum_cut_length < b.sum_cut_length)
-				return true;
-			return false;
-		}
-
-		virtual ~criteria(void) {}
-	};
-	
 	class gilotine {
-		t_other_sizes_list* pperebor_sizes;
-		t_other_sizes_list::iterator perebor_i;
-		scalar perebor(scalar size);		
-		void recursive(const t_list &list, t_stat &stat, unsigned s);
-		void bylen_bywid(const t_list &list, t_stat &stat);
+		perebor_2d perebor_2d;
 
-		void make_materials_list(void);		
-		void make_sizes_lists_for_material(const t_material &);
-		std::auto_ptr<criteria> pcriteria;
-		t_stat statistic;
-		t_sizes_list sizes[2];
-		struct {scalar size; scalar other_size;} minimum_size[2];
-		t_materials_list materials;
+		criteria default_criteria;
+		const criteria* pcriteria;
+
+		t_amounts remains;
+
+		t_sizes sizes[2];
+		scalar minimum_size[2];
+
+		t_parts sheets;
+
+		t_stat common_stat;
+
+		void remove_exosted_sizes(void);
+		bool make_one_raskroy_result(t_result&);
+
 	public:
-		t_factory factory;
-		t_parts_list parts;
-		t_parts_list lists;
+		gilotine(void);
 
-		gilotine(const t_parts_list &parts, const t_parts_list &lists, const t_factory &factory/*, criteria *pcriteria*/);
-		void run(void);
-		t_stat getStat(void) {return statistic;};
+		void set_factory(const t_factory&);
+		void set_recursion_max_depth(unsigned);
+		void set_monitor(monitor&);
+		void set_criteria(const criteria&);
+		t_stat get_common_stat(void);
+
+		bool first(const t_parts& parts, const t_parts& sheets, t_result&);
+		bool next(t_result&);
 	};
 }
 
