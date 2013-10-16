cb9c97c3b2065d19d1c4673113c24f7f79c9993b
Mikhail Denisenko <denisenkom@gmail.com>
2013-10-16 19:12:59 -0400

    added test with 2 details on sheet

--- test.py
+++ test.py
@@ -8,3 +8,10 @@ class TestCase(unittest.TestCase):
         self.assertDictEqual(
             {"along": 0, "elements": [{"type": 2, "rect": rect, "size": 10}]},
             res)
+
+        rect = {"size": (5, 10), "amount": 2}
+        print layout2d([rect], (10, 10))
+        #self.assertDictEqual(
+        #    {"along": 0, "elements": [{"type": 2, "rect": rect, "size": 5},
+        #                              {"type": 2, "rect": rect, "size": 5}]},
+        #    layout2d([rect], (10, 10)))
