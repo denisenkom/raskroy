c77fd7f1df2de68b4436f45e8d5ba3a44ba9a477
Mikhail Denisenko <denisenk@ncbi.nlm.nih.gov>
2013-10-17 15:21:58 -0400

    test improvements

--- test.py
+++ test.py
@@ -5,16 +5,22 @@ class TestCase(unittest.TestCase):
     def test_simple(self):
         rect = {"size": (10, 10)}
         res = layout2d([rect], (10, 10))
-        self.assertDictEqual(
-            {"along": 0, "elements": [{"type": 2, "rect": rect, "size": 10}]},
-            res)
+        self.assertIn(res["along"], (0, 1))
+        if res["along"] == 0:
+            self.assertDictEqual(
+                {"along": 0, "elements": [{"type": 2, "rect": rect, "size": 10}]},
+                res)
+        else:
+            self.assertDictEqual(
+                {"along": 1, "elements": [{"type": 2, "rect": rect, "size": 10}]},
+                res)
 
         rect = {"size": (5, 10), "amount": 2}
-        print layout2d([rect], (10, 10))
-        #self.assertDictEqual(
-        #    {"along": 0, "elements": [{"type": 2, "rect": rect, "size": 5},
-        #                              {"type": 2, "rect": rect, "size": 5}]},
-        #    layout2d([rect], (10, 10)))
+        self.assertDictEqual(
+            {"along": 0, "elements": [{"type": 2, "rect": rect, "size": 5},
+                                      {"type": 1, "size": 0},
+                                      {"type": 2, "rect": rect, "size": 5}]},
+            layout2d([rect], (10, 10)))
 
     def test_skinny_detail(self):
         # +-+---------+
