86ddcedbcccf47c5300e70e7c0400484f4234fa7
Mikhail Denisenko <denisenkom@gmail.com>
2013-10-18 01:59:29 -0400

    added CuttingOptimizer2 project which is using Platform Invoke interop mechanism instead of c++ cli which is CuttingOptimizer is using. Advantage of this is that whole code is now in C#.

---
+++ CuttingOptimizer2/CuttingOptimizer.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+
+namespace Denisenko.Cutting.Optimizing {
+    using Scalar = Int64;
+
+    public enum ElementType
+    {
+        Remain = 0,
+        Cut = 1,
+        Rect = 2,
+        SubLayout = 3,
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct _LayoutRect
+    {
+        public Scalar size_x;
+        public Scalar size_y;
+        public Int32 can_rotate;
+        public UInt32 amount;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    unsafe public struct _LayoutElement
+    {
+        [FieldOffset(0)]
+        public Scalar size;
+        [FieldOffset(8)]
+        public Int32 type;
+        [FieldOffset(16)]
+        public _Layout * layout;
+        [FieldOffset(16)]
+        public Int32 rect_index;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct _Layout
+    {
+        public Int32 along;
+        public UIntPtr num_elements;
+        unsafe public _LayoutElement * elements;
+    }
+
+
+    public class _GuillotineApi
+    {
+        [DllImport("guillotine_interop.dll")]
+        unsafe public static extern Int32 new_layout2d(
+            [MarshalAs(UnmanagedType.LPArray, SizeParamIndex=1)]_LayoutRect[] rects, UInt32 num,
+            Scalar sheet_x, Scalar sheet_y, Scalar cut_size,
+            _Layout ** result);
+
+        [DllImport("guillotine_interop.dll")]
+        unsafe public static extern void free_layout(_Layout * layout);
+
+        public static Decimal FromScaled(Int64 scaled)
+        {
+	        return (decimal)scaled / 1000;
+        }
+
+        public static Int64 ToScaled(Decimal value)
+        {
+	        return decimal.ToInt64(value * 1000);
+        }
+
+	    unsafe public static CuttingScheme ConvertLayout(_Layout * layout)
+	    {
+		    CuttingScheme result = new CuttingScheme();
+		    CuttingResultBuilder builder = new CuttingResultBuilder();
+		    builder.LoadSections(layout, result);
+		    return result;
+	    }
+    }
+
+
+	public class CannotCutDetailsException : Exception
+	{
+	public CannotCutDetailsException(String message)
+			: base(message)
+		{
+		}
+	};
+
+
+	public class Optimizer
+	{
+		public Optimizer()
+		{
+			//m_started = false;
+			m_parts = new List<Part>();
+			m_sheets = new List<Sheet>();
+			m_parameters = new ParametersCollection();
+		}
+
+		public List<Part> Parts { get { return m_parts; } }
+		public List<Sheet> Sheets { get { return m_sheets; } }
+		public ParametersCollection Parameters { get { return m_parameters; } }
+		public CuttingScheme CurrentResult { get { return m_result; } }
+		public Single PercentCompleted { get { return 0; } } // thread safe
+
+		/*public void Reset()
+		{
+			m_started = false;
+		}*/
+
+        public CuttingScheme Layout2d(List<Part> parts, Sheet sheet, Decimal cut_size)
+        {
+            var rects = new _LayoutRect[parts.Count];
+            for (var i = 0; i < parts.Count; i++) {
+                rects[i].amount = (uint)parts[i].Quantity;
+                rects[i].can_rotate = parts[i].CanRotate ? 1 : 0;
+                rects[i].size_x = _GuillotineApi.ToScaled(parts[i].Length);
+                rects[i].size_y = _GuillotineApi.ToScaled(parts[i].Width);
+            }
+            unsafe 
+            {
+                _Layout * layout;
+                var ret =_GuillotineApi.new_layout2d(rects, (uint)rects.Length,
+                    _GuillotineApi.ToScaled(sheet.Width), _GuillotineApi.ToScaled(sheet.Height),
+                    _GuillotineApi.ToScaled(cut_size), &layout);
+
+                try
+                {
+                    if (ret == 0)
+                    {
+                        return null;
+                    }
+                    else
+                    {
+                        var scheme = _GuillotineApi.ConvertLayout(layout);
+                        scheme.Sheet = sheet;
+                        // TODO:
+		                //scheme.Parameters = parameters;
+                        for (var i = 0; i < parts.Count; i++)
+                        {
+                            parts[i].Quantity = (int)rects[i].amount;
+                        }
+                        return scheme;
+                    }
+                }
+                finally
+                {
+                    _GuillotineApi.free_layout(layout);
+                }
+            }
+        }
+
+		public Boolean NextResult() 
+		{
+            //m_started = true;
+            if (m_parts.Count == 0)
+                throw new CannotCutDetailsException("Нельзя расположить детали, список деталей не заполнен.");
+            if (m_sheets.Count == 0)
+                throw new CannotCutDetailsException("Нельзя расположить детали, список листов не заполнен.");
+            CuttingScheme best_result = null;
+            foreach (var sheet in m_sheets)
+            {
+                var result = Layout2d(m_parts, sheet, Parameters.CutterThickness);
+                if (object.ReferenceEquals(best_result, null))
+                {
+                    best_result = result;
+                }
+                else
+                {
+                    if (result.BetterThan(best_result))
+                        best_result = result;
+                }
+            }
+            m_result = best_result;
+            return !object.ReferenceEquals(best_result, null);
+		}
+
+		private List<Part> m_parts;
+		private List<Sheet> m_sheets;
+		//private Boolean m_started;
+		private ParametersCollection m_parameters;
+		private CuttingScheme m_result;
+	};
+}
---
+++ CuttingOptimizer2/CuttingResultBuilder.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Denisenko.Cutting;
+
+namespace Denisenko.Cutting.Optimizing {
+
+public class CuttingResultBuilder
+{
+	public CuttingResultBuilder()
+	{
+	}
+
+	unsafe public void LoadSections(_Layout * input, CuttingScheme output)
+	{
+		m_result = output;
+		Recursive(input, output.RootSection);
+	}
+
+	private CuttingScheme m_result;
+
+	unsafe private void Recursive(_Layout * input, Section output)
+	{
+        CutType cutType = input->along == 0 ? CutType.Vertical : CutType.Horizontal;
+		m_result.MakeSureEdgeEven(ref output, cutType);
+
+        for (uint i = 0; i < (uint)input->num_elements; i++)
+        {
+            if (input->elements[i].type == (int)ElementType.Cut)
+                continue;
+            else if (input->elements[i].type == (int)ElementType.Remain)
+                continue;
+            var this_sec = m_result.Cut(output, _GuillotineApi.FromScaled(input->elements[i].size), cutType, out output);
+            if (input->elements[i].type == (int)ElementType.Rect)
+            {
+                m_result.MarkAsPart(this_sec);
+            }
+            else if (input->elements[i].type == (int)ElementType.SubLayout)
+            {
+                Recursive(input->elements[i].layout, this_sec);
+            }
+        }
+	}
+};
+
+}
